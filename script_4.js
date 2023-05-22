338054ff285a7f8b470590655474639512f84133
Alexandr770 <ExoTech@yandex.ru>
2023-05-22 20:55:25 +0300

    first commit

---
+++ script_4.js
@@ -0,0 +1,33 @@
+//1
+
+let i = 1;
+
+let n = Number (prompt('Привет'));
+
+while ( i <= n) {
+    alert('Привет'); 
+    i++
+}
+
+//2
+
+let i = 1;
+
+do {
+  console.log(i);
+  i++
+} while ( i < 6);
+
+//3
+
+
+let i = 7;
+
+do {
+  console.log(i);
+  i++
+} while ( i < 23);
+
+
+//4
+
